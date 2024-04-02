3e82fc62bf1e8b8c79e5c9c00b55bfc5a9343ba2
MisterStank <chakkritchongkraichak@gmail.com>
2024-04-03 01:07:41 +0700

    feat: :sparkles: video

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,6 +2,24 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
+interface Studio {
+  id: number;
+  name: string;
+  filtered_name: string;
+  real: boolean;
+  image: string;
+}
+
+interface Video {
+  id: number;
+  url: string;
+  image_url: string;
+  player_url: string;
+  name: string;
+  kind: string;
+  hosting: string;
+}
+
 interface Anime {
   name: string;
   episodes: number;
@@ -12,6 +30,10 @@ interface Anime {
     preview: string;
   };
   kind: string;
+  rating: string;
+  status: string;
+  studios: Studio[];
+  videos: Video[];
 }
 
 function AnimeDetail({ params }: { params: { id: number } }) {
@@ -31,6 +53,10 @@ function AnimeDetail({ params }: { params: { id: number } }) {
         score: data.score,
         image: data.image,
         kind: data.kind,
+        rating: data.rating,
+        status: data.status,
+        studios: data.studios,
+        videos: data.videos,
       });
     } catch (error) {
       console.error("Error fetching anime:", error);
@@ -46,20 +72,38 @@ function AnimeDetail({ params }: { params: { id: number } }) {
   }
 
   return (
-    <div className="flex flex-col items-center h-[89dvh]">
-      <div className="relative w-[225px] h-[320px]">
-        <Image
-          src={`https://shikimori.one${anime.image.original}`}
-          alt={anime.name}
-          fill
+    <div className="flex flex-col items-center h-[89dvh] pt-9 gap-y-5">
+      <div className="flex flex-row gap-x-20">
+        <div className="relative w-[225px] h-[320px]">
+          <Image
+            src={`https://shikimori.one${anime.image.original}`}
+            alt={anime.name}
+            fill
+            className="rounded-xl"
+          />
+        </div>
+        <iframe
+          width="420"
+          height="315"
+          src={`${anime.videos[0].player_url}?autoplay=1&mute=1&loop=1`}
+          allowFullScreen
+          allow="autoplay"
           className="rounded-xl"
-        />
+        ></iframe>
       </div>
-      <h1 className="font-semibold text-xl">{anime.name}</h1>
-      <div className="py-1 px-2 bg-[#161921] rounded-sm">
-        <p className="text-white text-sm font-bold capitalize">{anime.kind}</p>
+
+      <div className="flex flex-row">
+        <h1 className="font-semibold text-3xl">{anime.name}</h1>
+        <div className="py-1 px-2 bg-[#161921] rounded-sm">
+          <p className="text-white text-sm font-bold capitalize">
+            {anime.kind}
+          </p>
+        </div>
       </div>
-      <p>Episode: {anime.episodes || anime.episodes_aired}</p>
+      <p>Studios: {anime.studios[0].name}</p>
+      <p>Episodes: {anime.episodes || anime.episodes_aired}</p>
+      <p>Rating: {anime.rating}</p>
+      <p>Status: {anime.status}</p>
       <p>Score: {anime.score}</p>
     </div>
   );
